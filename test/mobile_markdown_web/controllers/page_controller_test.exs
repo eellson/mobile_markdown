17bd96dc26e2e3e27ce6f1f15c25325be416be3c
Ed Ellson <eellson@gmail.com>
2018-02-05 12:52:59 -0600

    Fix PageControllerTest
    This test has been failing for too long.

--- test/mobile_markdown_web/controllers/page_controller_test.exs
+++ test/mobile_markdown_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule MobileMarkdownWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "<div id=\"elm-target\">"
   end
 end
